ad7a9dadcedddede7f956411fcc0c3e0cd8c544a
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2024-10-09 14:18:25 -0500

    added some xml

--- app/src/main/java/fvtc/edu/grocerylist/MainActivity.java
+++ app/src/main/java/fvtc/edu/grocerylist/MainActivity.java
@@ -1,6 +1,10 @@
 package fvtc.edu.grocerylist;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_ShowMasterList)
+        {
+
+        }
+        else if (id == R.id.action_ShowShoppingList) {
+
+        }
+        else if (id == R.id.action_AddItem) {
+
+        }
+        else if(id == R.id.action_ClearAll){
+
+        }
+        else if(id == R.id.action_DeleteChecked){
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
