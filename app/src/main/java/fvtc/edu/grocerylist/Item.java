49f229efff32ab88f36c72dad2cd2c6d814fd736
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2024-10-09 14:23:20 -0500

    added Item.java

---
+++ app/src/main/java/fvtc/edu/grocerylist/Item.java
@@ -0,0 +1,52 @@
+package fvtc.edu.grocerylist;
+
+public class Item {
+    private int Id;
+    private String Description;
+    private boolean IsOnShoppingList;
+    private boolean IsInCart;
+    public Item(int id, String description, boolean isOnShoppingList, boolean isInCart){
+        Id = id;
+        Description = description;
+        IsOnShoppingList = isOnShoppingList;
+        IsInCart = isInCart;
+    }
+    public Item(){
+        Id = -1;
+        Description = "";
+        IsOnShoppingList = false;
+        IsInCart = false;
+    }
+    public String toString(){ return Id + "|" + Description + "|" + IsOnShoppingList + "|" + IsInCart;}
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public boolean isOnShoppingList() {
+        return IsOnShoppingList;
+    }
+
+    public void setOnShoppingList(boolean onShoppingList) {
+        IsOnShoppingList = onShoppingList;
+    }
+
+    public boolean isInCart() {
+        return IsInCart;
+    }
+
+    public void setInCart(boolean inCart) {
+        IsInCart = inCart;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+}
